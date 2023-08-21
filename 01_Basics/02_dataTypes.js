96a414c998c35dda21f85180332475692b48498e
Saadsohail27 <138296462+Saadsohail27@users.noreply.github.com>
2023-08-21 14:51:16 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) //we are using node JS, not browser
+
+console.log( 3 
+    + 
+    3) //code readibility should be high
+
+console.log("saad")
+
+
+let name = "saad"
+let age = 17
+let isLoggedIn = false
+
+
+ // number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined => 
+ //symbol => unique
+
+
+ //object
+
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object
